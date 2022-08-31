cbf3b95a7b0db498aecd14c193b9886488ee92d8
Marcynha01 <94566644+Marcynha01@users.noreply.github.com>
2022-08-31 15:23:48 -0300

    Add files via upload

---
+++ src/components/Doces/doces.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Menu from './Menu'
+
+const Doces = () => {
+    return (
+        <div className='container'>
+            <h2>Doces</h2>
+            <Menu/>
+
+        </div>
+    )
+}    
+
+export default Doces
